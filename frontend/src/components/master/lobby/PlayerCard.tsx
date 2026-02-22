b9ca767ef11921b8d896efddca2de019102965d6
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-02-22 15:14:05 +0100

    Update PlayerCard.tsx

--- frontend/src/components/master/lobby/PlayerCard.tsx
+++ frontend/src/components/master/lobby/PlayerCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./PlayerCard.module.css";
+//import styles from "./PlayerCard.module.css";
 import type { LobbyPlayer } from "../../../store/lobbyStore";
 
 function labelForStatus(p: LobbyPlayer): { text: string; className: string } {

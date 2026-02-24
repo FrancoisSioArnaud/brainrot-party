f5007b0233faea0090050e6c02429695505b01e6
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-02-24 03:17:26 +0100

    Create vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@contracts": path.resolve(__dirname, "../contracts"),
+    },
+  },
+});

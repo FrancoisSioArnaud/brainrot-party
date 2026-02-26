b4c0d8af59bdb29271339227dc3d5b605d1c1c00
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-02-26 23:03:25 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,9 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Landing from "./pages/Landing";
 import MasterSetup from "./pages/master/Setup";
 import MasterLobby from "./pages/master/Lobby";
+import MasterGame from "./pages/master/Game";
 import PlayEnter from "./pages/play/Enter";
+import PlayGame from "./pages/play/Game";
 
 export default function App() {
   return (
@@ -14,8 +16,10 @@ export default function App() {
 
         <Route path="/master/setup" element={<MasterSetup />} />
         <Route path="/master/lobby" element={<MasterLobby />} />
+        <Route path="/master/game" element={<MasterGame />} />
 
         <Route path="/play" element={<PlayEnter />} />
+        <Route path="/play/game" element={<PlayGame />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>

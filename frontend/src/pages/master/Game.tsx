5ca33947721106e53ec8c416ba8195f76435b0e6
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-02-27 23:09:45 +0100

    Update Game.tsx

--- frontend/src/pages/master/Game.tsx
+++ frontend/src/pages/master/Game.tsx
@@ -302,7 +302,7 @@ export default function MasterGame() {
               <div className="row" style={{ justifyContent: "space-between", alignItems: "center" }}>
                 <div style={{ display: "flex", flexDirection: "column"}}>
                   <div className="h1" style={{ margin: 0 }}>
-                    Brainrot-party.com
+                    Brainrot Party - Game
                   </div>
                   <div className="small mono">
                     {state?.room_code ?? session.room_code}
